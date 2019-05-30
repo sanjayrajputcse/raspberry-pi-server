f21273beee8657edc817470fac783726a3606292
Somil <somil.jain@blr-etv-jq2n542.fkipl.flipkart.com>
2019-05-31 04:04:05 +0530

    updating search marketplace

--- src/main/java/com/moksha/raspberrypi/server/Runner.java
+++ src/main/java/com/moksha/raspberrypi/server/Runner.java
@@ -18,17 +18,15 @@ import java.util.List;
 public class Runner {
     public static void main(
             String[] args) throws IOException {
-        SearchService callSearch = new SearchService();
         GetFKDetails getFKDetails = new GetFKDetails();
         final List<Product> cap = getFKDetails.searchKeyWordsAndReturnProducts("cap");
-        CollectionService collectionService = new CollectionService();
         List<String> toAddListingIds = new ArrayList<String>();
         List<String> toRemoveListingIds = new ArrayList<String>();
         toAddListingIds.add(cap.get(0).getListingId());
-        CollectionRequest collectionRequest = new CollectionRequest("HACK", toAddListingIds, toRemoveListingIds);
-        CollectionResponse newCollection = getFKDetails.createCollection(collectionRequest);
-        System.out.printf("\n collectionid : "+newCollection.getCollectionId());
-        System.out.printf("\n collectionURL: "+newCollection.getCollectionUrl());
+//        CollectionRequest collectionRequest = new CollectionRequest("HACK", toAddListingIds, toRemoveListingIds);
+//        CollectionResponse newCollection = getFKDetails.createCollection(collectionRequest);
+//        System.out.printf("\n collectionid : "+newCollection.getCollectionId());
+//        System.out.printf("\n collectionURL: "+newCollection.getCollectionUrl());
 
         String collectionId = "h2pg3ulcx0";
         CollectionRequest collectionRequestUpdate = new CollectionRequest(toAddListingIds, toRemoveListingIds);
