4a37b4dc3ffabf421136e8b4cd3036c5244f0fa9
Somil <somil.jain@blr-etv-jq2n542.fkipl.flipkart.com>
2019-05-31 08:01:18 +0530

    updating word in the query

--- src/main/java/com/moksha/raspberrypi/server/fkService/SearchService.java
+++ src/main/java/com/moksha/raspberrypi/server/fkService/SearchService.java
@@ -27,7 +27,7 @@ public class SearchService {
 
     public List<Product> searchOnAKeyword(String word) throws IOException {
         List<Product> listOfProduct = new ArrayList<>();
-        String url = HOST + API ;
+        String url = HOST + API + word ;
 //        System.out.println(url);
         Request request = new Request.Builder()
                 .url(url)
